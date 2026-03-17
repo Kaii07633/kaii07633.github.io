1f381b4d68ded3fc16fb87f716e4bae624b2e8bf
NeonveresVh <gurawshark6245@gmail.com>
2026-03-17 20:24:36 +0700

    Create api/constants.js

---
+++ api/constants.js
@@ -0,0 +1,9 @@
+module.exports = {
+    ERR_CODE: {
+        UNKNOWN: "E000",
+        AUTH_FAIL: "E101",
+        TAMPERED: "E202",
+        FLOOD: "E303"
+    },
+    SYSTEM_NAME: "NEON_CORE_SHIELD"
+};
